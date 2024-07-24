3002999179f47bec65c1c77213f6d61c829ce958
NazarKopach <164062094+NazarKopach@users.noreply.github.com>
2024-07-24 17:09:11 +0200

    add task1,2,3,4,5,6

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,9 @@
-function makeTransaction(quantity, pricePerDroid) {
-    return (`You ordered ${quantity} droids worth ${quantity*pricePerDroid} credits!`);
-}
-
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+const categoriesList = document.querySelector('#categories');
+const categoryItems = categoriesList.querySelectorAll('.item');
+console.log(`Number of categories: ${categoryItems.length}`);
+categoryItems.forEach(item => {
+  const categoryTitle = item.querySelector('h2').textContent;
+  const categoryElements = item.querySelectorAll('ul li');
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${categoryElements.length}`);
+});
